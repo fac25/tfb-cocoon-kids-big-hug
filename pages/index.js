68083b9e81dae12a05d55cb893fb3e7495537dd6
lisahns <lisa.hanes@hotmail.de>
2022-11-28 12:46:21 +0000

    added db structure

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
-export default function Landing() {
+export async function getStaticProps() {
+    const data = await fetch('http://localhost:3002/emotions')
+    const emotion = await data.json()
+    return { props: { emotion } }
+}
+
+export default function Landing({ emotion }) {
+    console.log(emotion)
     return <div>Hello</div>
 }
