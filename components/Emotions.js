5e842baff8e9fdcb8dee35d3aa02540ed412877c
karwanm <karwan.ismo@gmail.com>
2022-11-28 19:22:24 +0000

    emotions component

---
+++ components/Emotions.js
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+
+const Emotions = ({ emotions }) => {
+    return (
+        <div>
+            <h1>Emotions</h1>
+            {emotions.map((emotion) => {
+                return (
+                    <div key={emotion.id}>
+                        <Image
+                            src="/assets/img/over13/logo.jpg"
+                            width={300}
+                            height={300}
+                        />
+                        <p key={emotion.id}>{emotion.name}</p>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Emotions
