b305e6a8195f688070eb630513d97bf315c2aa40
karwanm <karwan.ismo@gmail.com>
2022-11-29 15:08:39 +0000

    emotions page and single emotion page

--- pages/user-group/index.js
+++ pages/user-group/index.js
@@ -1,12 +1,13 @@
+
 import Emotions from '../../components/Emotions'
 
 export async function getStaticProps() {
     const res = await fetch('http://localhost:3002/emotions')
     const emotions = await res.json()
+
     return { props: { emotions } }
 }
 
-export default function AllEmotions({ emotions }) {
-    // console.log(localStorage && localStorage.getItem("user-group"))
-    return <Emotions emotions={emotions} />
+export default function UserGroup({ emotions }) {
+    return <Emotions emotions={emotions} length={9} page={true} />
 }
