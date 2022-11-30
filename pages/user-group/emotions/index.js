7700f29342e8a608de2d87c290bb2eeac4a0d550
lisahns <lisa.hanes@hotmail.de>
2022-11-30 17:33:24 +0000

    added searchbar to emotions page Relates #11

--- pages/user-group/emotions/index.js
+++ pages/user-group/emotions/index.js
@@ -1,15 +1,31 @@
 import Emotions from '../../../components/Emotions'
 import Layout from '../../../components/Layout'
+import Select from 'react-select'
+import { useState } from 'react'
+import Link from 'next/link'
 
 export async function getStaticProps() {
     const res = await fetch('http://localhost:3003/emotions')
     const emotions = await res.json()
-    return { props: { emotions } }
+    const emotionsArray = emotions.map((el) => el.name)
+    console.log(emotionsArray)
+    const selectOptions = emotionsArray.map((opt) => ({
+        label: opt,
+        value: opt,
+    }))
+    console.log(selectOptions)
+    return { props: { emotions, selectOptions } }
 }
 
-export default function AllEmotions({ emotions }) {
+export default function AllEmotions({ emotions, selectOptions }) {
+    const [userChoice, setUserChoice] = useState('')
     return (
         <Layout pageTitle="Emotions">
+            <Select
+                options={selectOptions}
+                onChange={(opt) => setUserChoice(opt.value)}
+            />
+            <Link href={userChoice}>SEARCH</Link>
             <Emotions
                 emotions={emotions}
                 length={emotions.length}
