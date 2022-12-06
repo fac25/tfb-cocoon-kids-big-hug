b2c5946137cdad740ae6d74825a261634ce350b0
flare-s <webflarez@gmail.com>
2022-12-06 12:05:41 +0000

    Contact us content
    Co-authored-by: Lisa <lisahns@users.noreply.github.com>

--- src/pages/contactus.js
+++
@@ -1,8 +0,0 @@
-import Layout from '../components/layout/Layout'
-export default function ContactUs() {
-    return (
-        <Layout pageTitle="Contact us">
-            <div>about us</div>
-        </Layout>
-    )
-}
