8a63dfd24d04da80a26eab2ec5fb6d0e0d969f9f
mariapaz <mariapazgar@gmail.com>
2022-12-07 14:43:38 +0000

    added styles domakes

--- src/components/domake/DoMake.js
+++ src/components/domake/DoMake.js
@@ -1,61 +1,89 @@
 import Image from 'next/image'
-
+import styles from '../../styles/Single.module.css'
 // import { useContext } from 'react'
 // import GlobalUserGroup from '../lib/GlobalContext'
 
 const DoMake = ({ domake }) => {
     return (
-        <article>
-            <h2>{domake.title}</h2>
-            <p>{domake.description}</p>
-            <Image
-                src={`/assets/img/${domake.src}`}
-                alt={domake.title}
-                height={300}
-                width={300}
-            ></Image>
-            <div>
-                <h3>How it Helps</h3>
-                <ul>
-                    {domake['how-it-helps'].map((how) => {
-                        return <li key={how}>{how}</li>
-                    })}
-                </ul>
-            </div>
-            <h3>Caution!</h3>
-            <p>{domake.caution}</p>
-            {domake['materials'] && (
-                <div>
-                    <h3>Materials</h3>
-                    <ul>
-                        {domake['materials']?.map((item) => (
-                            <li key={item}>{item}</li>
-                        ))}
-                    </ul>
-                </div>
-            )}
-            {domake['instructions'] && (
-                <div>
-                    <h3>Instructions</h3>
-                    <div>
-                        {domake['instructions']?.map((item, index) => (
-                            <div key={`item-${index}`}>
-                                <ul>
-                                    {item.step?.map((item) => (
-                                        <li key={item}>{item}</li>
-                                    ))}
-                                </ul>
-                                <Image
-                                    src={`/assets/img/${item.src}`}
-                                    alt={item.title}
-                                    height={150}
-                                    width={150}
-                                ></Image>
-                            </div>
-                        ))}
+        <article className="container">
+            <header>
+                <h1 className="title">{domake.title}</h1>
+            </header>
+            <main className={styles.wrapper}>
+                <header className={styles.header}>
+                    {domake.src && (
+                        <div className={styles.header__image}>
+                            <Image
+                                src={`/assets/img/do/${domake.src}`}
+                                alt={domake.title}
+                                height={300}
+                                width={300}
+                            ></Image>{' '}
+                        </div>
+                    )}
+
+                    <div className={styles.header__info}>
+                        <p className="lead">{domake.description}</p>
                     </div>
-                </div>
-            )}
+                </header>
+                <section>
+                    <h2>How it Helps</h2>
+                    <ul>
+                        {domake['how-it-helps']?.map((how) => {
+                            return <li key={how}>{how}</li>
+                        })}
+                    </ul>
+                </section>
+
+                {domake.caution ? (
+                    <div className="caution">{domake.caution}</div>
+                ) : (
+                    ''
+                )}
+                {domake['materials'] && (
+                    <section>
+                        <h3>What you need</h3>
+                        <ul className="list-style">
+                            {domake['materials']?.map((item) => (
+                                <li key={item}>{item}</li>
+                            ))}
+                        </ul>
+                    </section>
+                )}
+                {domake['instructions'] && (
+                    <section>
+                        <h3>What to do</h3>
+                        <div className={styles.mb2}>
+                            {domake['instructions']?.map((item, index) => (
+                                <div
+                                    key={`item-${index}`}
+                                    className={styles.flex__wrapper}
+                                >
+                                    <div className={styles.flex__image}>
+                                        {item.src ? (
+                                            <Image
+                                                src={`/assets/img/do/${item.src}`}
+                                                alt={item.title}
+                                                height={150}
+                                                width={150}
+                                            ></Image>
+                                        ) : (
+                                            ''
+                                        )}
+                                    </div>
+                                    <div className={styles.flex__info}>
+                                        <ol>
+                                            {item.step?.map((item) => (
+                                                <li key={item}>{item}</li>
+                                            ))}
+                                        </ol>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </section>
+                )}
+            </main>
         </article>
     )
 }
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="wrapper__footer">
             <div className="container">
-                <p>Cocoon Kids and Big Hug &#169; 2022</p>
+                <p>Cocoon Kids & Big Hug &#169; 2022</p>
             </div>
         </footer>
     )
